2153b8094121362a0637341af9dc75618d9d0e71
maykonmarcosjuniordev-star <maykon.marcos.junior.dev@gmail.com>
2025-04-23 18:28:03 -0300

    fixin graph bug, and organizing in a better suited way

--- src/shap_custom.py
+++ src/shap_custom.py
@@ -9,29 +9,43 @@ sys.path.append(project_dir)
 ###
 import shap
 import matplotlib.pyplot as plt
-
-from src.training import get_models
+from joblib import load
 from src.file_op import load_data
 
-def map_sentences_idx(X_test, sentencas_test, numero_sentenca:list[int]) -> list[int]:
-    # Busca os índices em que essa sentença aparece no X_test
-    idxs = []
-    for num in numero_sentenca:
-        idx = sentencas_test[sentencas_test == num].index.tolist()[0]
-        idxs.append(X_test.index.get_loc(idx))
-    return idxs
 
-def explain_prediction(shap_values, N_features, sent_num:list[int], sent_pos:list[int]):
+def get_values(model, X_train, X_test) -> shap.Explanation:
+    """
+    Calculate SHAP values for a given model and dataset.
+    Parameters:
+    - model: The machine learning model to explain.
+    - X_train: The training data.
+    - X_test: The test data.
+    Returns:
+    - shap_values: The SHAP values for the test data.
+    """
+
+    # Create a SHAP explainer
+    explainer = shap.Explainer(model, X_train)
+    # Calculate SHAP values
+    shap_values = explainer(X_test)
+
+    return shap_values
+
+
+def map_sentences_idx(X_test, sentencas_test, numero_sentenca:int) -> int:
+    # Busca os índices em que essa sentença aparece no X_test
+    idx = sentencas_test[sentencas_test == numero_sentenca].index.tolist()[0]
+    return X_test.index.get_loc(idx)
+
+
+def explain_prediction(shap_values, N_features, sent_num:int, sent_pos:int):
     """
     mostrando o valor base, cada contribuição das variáveis, e o valor final previsto.
     """
-    N = len(sent_pos)
-    for i in range(N):
-        # Gráfico de barras
-        shap.plots.bar(shap_values[sent_pos[i]], show=False, max_display=N_features)
-        plt.title(f"SHAP - Sentença {sent_num[i]} - Posição {sent_pos[i]}")
-        plt.savefig(f"shap_sentenca_{sent_num[i]}.png", bbox_inches="tight", dpi=300)
-        plt.clf()
+    shap.plots.waterfall(shap_values[sent_pos], show=False, max_display=N_features)
+    plt.title(f"SHAP - Sentença {sent_num}")
+    plt.savefig(f"shap_sentenca_{sent_num}.png", bbox_inches="tight", dpi=300)
+    plt.clf()
 
 
 def explain_global(shap_values, N_features):
@@ -47,51 +61,34 @@ def explain_global(shap_values, N_features):
     plt.clf()
     return 0
 
-def custom_shap(model, X_train, X_test, y_train) -> shap.Explanation:
-    """
-    Calculate SHAP values for a given model and dataset.
-    Parameters:
-    - model: The machine learning model to explain.
-    - X_train: The training data.
-    - X_test: The test data.
-    - y_train: The target variable for the training data.
-    Returns:
-    - shap_values: The SHAP values for the test data.
-    """
-    # Fit the base model
-    model.fit(X_train, y_train)
 
-    # Create a SHAP explainer
-    explainer = shap.Explainer(model, X_train)
-    # Calculate SHAP values
-    shap_values = explainer(X_test)
-
-    return shap_values
-
-
-def main():
-    # receive the arguments from the command line
-    args = sys.argv[1:]
-
+def custom_shap(model_path='models_storage/DecisionTree.pkl', sent_num=0):
     # Importar o modelo base
-    model = get_models()
-    base_model = model['DecisionTree']
+    base_model = load(model_path)
     # Carregar os dados
-    X_train, X_test, y_train, _, _, _, sent_test = load_data(split=True)
+    X_train, X_test, _, _, _, _, sent_test = load_data(split=True)
     N_features = X_test.shape[1]
     # Calculate SHAP values
-    shap_values = custom_shap(base_model, X_train, X_test, y_train)
-
-    # check if the arguments are empty
-    if len(args) > 0 and args[0].isdigit():
-        # check if the first argument is a number
-        # convert the first argument to an integer
-        sent_num = int(args[0])
-        sent_pos = map_sentences_idx(X_test, [sent_test], [sent_num])
-        explain_prediction(shap_values, N_features, [sent_num], [sent_pos])
+    shap_values = get_values(base_model, X_train, X_test)
+    if sent_num > 0:
+        sent_pos = map_sentences_idx(X_test, sent_test, sent_num)
+        explain_prediction(shap_values, N_features, sent_num, sent_pos)
     else:
-        print("No valid arguments provided.\nWill explain globally")
         explain_global(shap_values, N_features)
 
 if __name__ == '__main__':
-    main()
+    # receive the arguments from the command line
+    args = sys.argv[1:]
+    # check if the arguments are empty
+    if len(args) == 0:
+        print("No arguments provided.\nusage: python shap_custom.py model_path [sentence_number]")
+        custom_shap()
+    elif len(args) > 1 and args[1].isdigit():
+        model_path = args[0]
+        sent_num = int(args[1])
+        print(f"Explaining sentence: {sent_num}")
+        custom_shap(model_path, sent_num)
+    else:
+        print("No sentence provided.\nWill explain globally")
+        model_path = args[0]
+        custom_shap(model_path)
