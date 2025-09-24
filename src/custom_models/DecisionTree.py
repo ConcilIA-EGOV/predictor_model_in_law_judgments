d2a47c764ad72d61ba761c21ad538714e7be71b5
maykonmarcosjuniordev-star <maykon.marcos.junior.dev@gmail.com>
2025-09-24 16:09:46 -0300

    regularizing random seed

--- src/custom_models/DecisionTree.py
+++ src/custom_models/DecisionTree.py
@@ -1,7 +1,9 @@
 from sklearn.tree import DecisionTreeRegressor
 
+from src.util.parameters import RANDOM_STATE
+
 def get_model():
     model = DecisionTreeRegressor(min_samples_split=2, max_features=1.0,
                                   criterion='poisson', max_depth=15,
-                                  random_state=15)
+                                  random_state=RANDOM_STATE)
     return model
